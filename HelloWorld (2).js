0a716657afd55f35a83a64fc50ef98c2608861ad
harshmishra31481 <95023043+harshmishra31481@users.noreply.github.com>
2022-01-29 17:44:04 +0530

    Add files via upload

---
+++ HelloWorld (2).js
@@ -0,0 +1,7 @@
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
